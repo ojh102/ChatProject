59e4cfaee1a267797814019270409a9058ee61fc
OH JAE HWAN <ojh102@naver.com>
2016-09-23 20:58:57 +0900

    Initial commit

---
+++ app/src/main/java/ojh102/github/com/chatproject/SpalshActivity.java
@@ -0,0 +1,13 @@
+package ojh102.github.com.chatproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+    }
+}
